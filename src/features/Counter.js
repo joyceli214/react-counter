157c52f6a8266a6bed422d85009583e3fd648187
lijo15 <lijo15@oocl.com>
2022-12-12 16:04:24 +0800

    increase and decrease button function

--- src/features/Counter.js
+++ src/features/Counter.js
@@ -1,13 +1,20 @@
 // react fucntional component: rfc
 
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Counter() {
+const [number, setNumber] = useState(0);
+const onIncrease = () => {
+    setNumber(number + 1);
+};
+const onDecrease = () => {
+    setNumber(number - 1);
+};
   return (
     <div>
-        <button>+</button>
-        <span>0</span>
-        <button>-</button>
+        <button onClick={onIncrease}>+</button>
+        <span>{number}</span>
+        <button onClick={onDecrease}>-</button>
     </div>
   )
 }

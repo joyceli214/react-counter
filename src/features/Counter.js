2f873f52ba66cbc33d14db6a4bb5ed29691d9840
lijo15 <lijo15@oocl.com>
2022-12-12 15:46:15 +0800

    Counter init

---
+++ src/features/Counter.js
@@ -0,0 +1,13 @@
+// react fucntional component: rfc
+
+import React from 'react'
+
+export default function Counter() {
+  return (
+    <div>
+        <button>+</button>
+        <span>0</span>
+        <button>-</button>
+    </div>
+  )
+}
